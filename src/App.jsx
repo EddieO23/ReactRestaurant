8bc919aab42ac123112c5c50b826fefc3c1e820f
Eddie <eocampo52@gmail.com>
2025-04-18 21:09:29 -0500

    work on react router dom side of project. added react router dom to project. need to set up navigation link layout and make pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <>
-      <h1>Clean Slate</h1>
+      <h1 className="heading-font  text-3xl font-bold">Clean Slate</h1>
     </>
   )
 }
